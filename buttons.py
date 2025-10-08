540da5227c9bd4cba7d457e0f60f6ebb43b8fff2
Hanna Kunatowska <hannamirakristina@gmail.com>
2025-10-08 11:22:55 +0200

    changed from RPi.GPIO to gpiozero

--- buttons.py
+++ buttons.py
@@ -1,53 +1,43 @@
-import RPi.GPIO as GPIO
-import time
+from gpiozero import Motor
+from time import sleep
 
-# --- GPIO Setup ---
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+# --- Motor setup ---
+# Map your pins to Motor objects
+# Adjust forward/backward pins according to your wiring
+forward_motor = Motor(forward=5, backward=6)
+turn_motor = Motor(forward=13, backward=19)
 
-# Define motor control pins
-motors = {
-    "forward": 5,
-    "backward": 6,
-    "left": 13,
-    "right": 19
-}
-
-# Setup pins as outputs
-for pin in motors.values():
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.LOW)
-
+# --- Functions ---
 def all_stop():
-    for pin in motors.values():
-        GPIO.output(pin, GPIO.LOW)
+    forward_motor.stop()
+    turn_motor.stop()
 
 def move_forward(duration=1):
     print("Moving forward")
     all_stop()
-    GPIO.output(motors["forward"], GPIO.HIGH)
-    time.sleep(duration)
+    forward_motor.forward()
+    sleep(duration)
     all_stop()
 
 def move_backward(duration=1):
     print("Moving backward")
     all_stop()
-    GPIO.output(motors["backward"], GPIO.HIGH)
-    time.sleep(duration)
+    forward_motor.backward()
+    sleep(duration)
     all_stop()
 
 def turn_left(duration=0.5):
     print("Turning left")
     all_stop()
-    GPIO.output(motors["left"], GPIO.HIGH)
-    time.sleep(duration)
+    turn_motor.forward()
+    sleep(duration)
     all_stop()
 
 def turn_right(duration=0.5):
     print("Turning right")
     all_stop()
-    GPIO.output(motors["right"], GPIO.HIGH)
-    time.sleep(duration)
+    turn_motor.backward()
+    sleep(duration)
     all_stop()
 
 # --- Example test sequence ---
@@ -59,9 +49,5 @@ try:
     all_stop()
 
 except KeyboardInterrupt:
-    pass
-
-finally:
-    print("Cleaning up GPIO...")
     all_stop()
-    GPIO.cleanup()
+    print("Interrupted!")
