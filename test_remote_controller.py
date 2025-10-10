9c582f1cb6b1895659eb5e4daacceab31b6203ed
vitusberinggg <virrebering@gmail.com>
2025-10-10 10:31:22 +0200

    Fixed remote controller test a bit

--- test_remote_controller.py
+++ test_remote_controller.py
@@ -6,24 +6,29 @@ import time
 
 # --- Definitions ---
 
-button_pins = []
+button_pins = [11, 13, 15, 16]
 
-move_forward_button_pin =
-move_backwards_button_pin =
-turn_right_button_pin =
-turn_left_button_pin =
+move_forward_button_pin = 15
+move_backwards_button_pin = 16
+turn_right_button_pin = 11
+turn_left_button_pin = 13
 
 # --- Setup ---
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 
 for button_pin in button_pins:
     GPIO.setup(button_pin, GPIO.IN)
 
 # --- Functions ---
 
-def press(button_pin):
+def press_without_transistor(button_pin):
     GPIO.setup(button_pin, GPIO.OUT, initial = GPIO.LOW)
+    time.sleep(0.5)
+    GPIO.setup(button_pin, GPIO.IN)
+
+def press_with_transistor(button_pin):
+    GPIO.setup(button_pin, GPIO.OUT, initial = GPIO.HIGH)
     time.sleep(0.5)
     GPIO.setup(button_pin, GPIO.IN)
 
@@ -31,19 +36,19 @@ def press(button_pin):
 
 print("Trying to move forward...")
 
-press(move_forward_button_pin)
+press_without_transistor(move_forward_button_pin)
 
 print("Trying to move backwards...")
 
-press(move_backwards_button_pin)
+press_with_transistor(move_backwards_button_pin)
 
 print("Trying to turn right...")
 
-press(turn_right_button_pin)
+press_with_transistor(turn_right_button_pin)
 
 print("Trying to turn left...")
 
-press(turn_left_button_pin)
+press_without_transistor(turn_left_button_pin)
 
 print("Testing done!")
 
