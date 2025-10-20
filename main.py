748328a1170533168e3c7f24ab485a2d1cc82477
vitusberinggg <virrebering@gmail.com>
2025-10-20 13:42:21 +0200

    changed print

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ def follow():
         distance_in_cm = get_distance() # Gets distance to closest obstacle from ultrasonic sensor
         
         if obstacle or distance_in_cm <= safe_distance_in_cm: # If either the AI camera or the ultrasonic sensor detects an obstacle:
-            print("\nObstacle detected, trying to avoid it...")
+            print("\Trying to avoid obstacle...")
             avoid_obstacle()
             continue
             
