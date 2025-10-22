ee71cc3271397592d54e14873bb13b8ad79092da
klarapalsson <klara.palsson@ericsson.com>
2025-10-22 11:44:03 +0200

    Update temp_obstacle_function.py

--- temp_obstacle_function.py
+++ temp_obstacle_function.py
@@ -1,1 +1,52 @@
-def 
+def avoid_obstacle():
+
+    """
+    Avoids an obstacle by stopping.
+
+    Arguments:
+        None
+
+    Returns:
+        None
+    
+    """
+
+    print("\nStopping...")
+    stop()
+    move_backwards(time)
+    #stop
+    #if person
+    # if centered
+    #check left
+    # if not obstacle 
+    #go_around_left()
+    #else
+    #check right 
+    #if not obstacle
+    # go around right 
+    
+    # if not person
+    #check left 
+    # if person and not obstacle
+    #follow
+    # elif not person and not obstacle 
+    # go_around_left()
+    # else 
+    #check right 
+    # if person and not obstacle
+    #follow
+    # elif not person and not obstacle 
+    # go_around_right()
+    # else 
+    #stop 
+def go_around_left()
+    move_forward()
+    turn("left", 180) 
+    turn("right",0) 
+
+def go_around_right()
+    move_forward()
+    turn("right",0) 
+    turn("left", 180) 
+    
+
