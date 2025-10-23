8d46db482c6301f79588acd9bfe37fe4346214fb
vitusberinggg <virrebering@gmail.com>
2025-10-23 10:03:10 +0200

    added button press check

--- remote_controller.py
+++ remote_controller.py
@@ -22,7 +22,46 @@ for button_pin in button_pins:
 # --- Functions ---
 
 def press(button_pin):
+
+    """
+    Simulates a button press by driving the GPIO pin HIGH.
+    
+    Arguments:
+        "button_pin": The GPIO pin of the button to press.
+        
+    Returns:
+        None
+    
+    """
+
     lgpio.gpio_claim_output(handle, button_pin, 1) # Drive the pin HIGH
 
 def unpress(button_pin):
+
+    """
+    Simulates a button release by setting the GPIO pin to input (high impedance).
+    
+    Arguments:
+        "button_pin": The GPIO pin of the button to release.
+        
+    Returns:
+        None
+    
+    """
+
     lgpio.gpio_claim_input(handle, button_pin) # Set the pin to input (high impedance)
+
+def check_button_press(button_pin):
+
+    """
+    Checks if a button is pressed.
+    
+    Arguments:
+        "button_pin": The GPIO pin of the button to check.
+    
+    Returns:
+        True if the button is pressed, False otherwise.
+    
+    """
+
+    return lgpio.gpio_read(handle, button_pin) == 1 # Returns True if the pin is HIGH (button pressed), else False
