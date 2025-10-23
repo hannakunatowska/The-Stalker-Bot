365d8ab255e0cb0cd7b94f1692a2e3e13045f223
Max Jäderström <max.jaderstrom@ericsson.com>
2025-10-23 15:54:00 +0200

    temp obstacle

--- temp_obstacle_function.py
+++ temp_obstacle_function.py
@@ -1,133 +1,132 @@
 import ai_detection
+import time
 from main import stop, move_backwards, move_forward, turn, follow
-
 
 def avoid_obstacle():
 
-    angle, direction, obstacle, person_height = ai_detection.get_tracking_data() # Gets necessary data from the AI camera
+     obstacle_note = False
+     timer = 0
+     angle, direction, obstacle, person_height = ai_detection.get_tracking_data() # Gets necessary data from the AI camera
 
-    """
-    Avoids an obstacle by stopping.
+     """
+     Avoids an obstacle by stopping.
 
-    Arguments:
-        None
+     Arguments:
+          None
 
-    Returns:
-        None
+     Returns:
+         None
     
-    """
-
-    print("\nStopping...")
-    stop()
-    move_backwards(time)
-
-
-    # stop
-
-    # if person
-    #   if centered
-
-    #       check left
-
-    #       if not obstacle 
-    #           go_around_left()
-
-    #       else
-    #           check right 
-
-    #           if not obstacle
-    #               go around right 
-
-    #               continue
+     """
     
-    print("\nStopping...")
-    stop()
+     print("\nStopping...")
+     stop()    # stop
 
-    if person_height is not None:
-         if direction == "centered":
+     print("\nMoving backwards...")
+     
+     timer = time.time()  # Start the timer
+     while time.time() - timer < 1:  # Move backwards for 1 seconds
+          move_backwards()
+     
+
+     if person_height is not None: # if person
+          if direction == "centered":   # if centered
               
-              print("\nChecking left...")
-              check_left
+               print("\nChecking left...")
+               check_left()   # check left
 
-              if not obstacle:
-                   print("\nObstacle not detected, going around...")
-                   go_around_left()
+               if not obstacle:    # if not obstacle 
+                    print("\nObstacle not detected, going around...")
+                    go_around_left()    # go_around_left()
 
-              else:
-                   print("\nNo person detected, checking right...")
-                   check_right
+               else:     # else obstacle   
+                    print("\nNo person detected, checking right...")
+                    check_right()  # check right 
 
-                   if not obstacle:
-                        print("\nNo obstacle detected, going around...")
-                        go_around_right()
-              
-                   
-              
+                    if not obstacle:    # if not obstacle
+                         print("\nNo obstacle detected, going around...")
+                         go_around_right()   # go around right 
 
-    # if not person
 
-    #   check left 
 
-    #   if person and not obstacle
-    #       follow
+     if person_height is None:     # if no person
+          if direction == "centered":
+               print("\nNotes if there is an obstacle or not")
+               obstacle_note = obstacle
 
-    #   elif not person and not obstacle 
-    #       go_around_left()
+               print("\nNo person detected, checking left...")
+               check_left()   # check left
 
-    #   else 
-    #       check right 
+               if ((person_height is not None) and (not obstacle)):   # if person and not obstacle
+                    print("\nPerson found, following...")
+                    follow()     # follow
+                    return
+               
+               elif ((person_height is not None) and (obstacle)):     # elif person and obstacle
 
-    #       if person and not obstacle
-    #           follow
+                    if obstacle_note:   # if theres an obstacle to the right
 
-    #       elif not person and not obstacle 
-    #           go_around_right()
+                         check_left()
 
-    #       else 
-    #           stop 
+                         print("\nPerson and obstacle detected, going around...")
+                         go_around_left()  # go around left
 
-    if person_height is None:
-            print("\nNo person detected, checking left...")
+                    if ((person_height is not None) and (not obstacle)):   # if person and not obstacle
+                         print("\nPerson found, following...")
+                         follow()     # follow
+                         return
 
-            check_left()
+               print("\nNo person to the left, checking right...")
+               check_right()  # check right
 
-            if ((person_height is not None) and (not obstacle)):
-                 print("\nPerson found, following...")
-                 follow()
-            
-            elif ((person_height is None) and (not obstacle)):
-                 print("\nNo person detected, no obstacle detected, going around...")
-                 go_around_left
+               if ((person_height is not None) and (not obstacle)):  # if person and not obstacle
+                    print("\nPerson found, following...")
+                    follow()     # follow
 
-            else:
-                 print("\nNo person to the left, checking right...")
-                 check_right
+               elif ((person_height is None) and (not obstacle)):    # elif no person and not obstacle
+                    print("\nNo person detected, no obstacle detected, going around...")
+                    go_around_right() # go around right
 
-                 if ((person_height is not None) and (not obstacle)):
-                      print("\nPerson found, following...")
-                      follow()
+                    print("\nChecking right...")
+                    check_right()  # check right
 
-                 elif ((person_height is None) and (not obstacle)):
-                      print("\nNo person detected, no obstacle detected, going around...")
-                      go_around_right
+                    if ((person_height is not None) and (not obstacle)):  # if person and not obstacle
+                         print("\nPerson found, following...")
+                         follow()     # follow
 
-                 else:
-                      print("\nNo persen detected, stopping...")
-                      stop()
+               else:     # else no person detected
+                    print("\nNo persen detected, stopping...")
+                    stop()   # stop
 
 
             
 
 
 def go_around_left():
-    move_forward()
-    turn("left", 180) 
-    turn("right",0) 
+
+     """
+
+     turn left until obstacle is not detected
+
+     possibly holding the obstacle to 45 or 90 degrees of car
+
+     """
+
+     move_forward()
+     turn("left", 180) 
+     turn("right",0) 
 
 def go_around_right():
-    move_forward()
-    turn("right",0) 
-    turn("left", 180) 
+
+     """
+
+     turn right until obstacle is not detected
+
+     """
+
+     move_forward()
+     turn("right",0) 
+     turn("left", 180) 
     
 def check_left():
 
